4c2ff4d412e1fa1ed619edc02ff3925f0e54df13
Coder-person <73622307+Coder-person@users.noreply.github.com>
2021-01-28 13:08:21 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function Tamil(){
+    document.getElementById('H1').innerHTML = "பிறந்தநாள் வாழ்த்துக்கள்";
+    document.getElementById('H2').innerHTML = "hApPy biRtHDY !! இன்று நீங்கள் உங்கள் நாளை அனுபவித்தீர்கள் என்று நம்புகிறேன், இன்று கேக் இருந்தது என்று நம்புகிறேன்.";
+    document.getElementById('Lang').innerHTML = "மொழிகள்";
+    document.getElementById('tamil').innerHTML = "தமிழ்";
+    document.getElementById('french').innerHTML = "பிரஞ்சு";
+    document.getElementById('english').innerHTML = "ஆங்கிலம்";
+}
+function French(){
+    document.getElementById('H1').innerHTML = "joyeux anniversaire appa";
+    document.getElementById('H2').innerHTML = "hApPy biRtHDY !! J'espère que vous avez apprécié votre journée aujourd'hui, et j'espère qu'il y a eu un gâteau aujourd'hui.aGAIn I WISH U a HApPy bIrtHday: DDDDD";
+    document.getElementById('Lang').innerHTML = "Langue";
+    document.getElementById('tamil').innerHTML = "Tamil";
+    document.getElementById('french').innerHTML = "Française";
+    document.getElementById('english').innerHTML = "Anglaise";
+}
+function English(){
+    document.getElementById('H1').innerHTML = "HAPPY BIRTHDAY APPA";
+    document.getElementById('H2').innerHTML = "hApPy biRtHDY !! I hope u enjoyed your day today, and hopefully there was cake today.aGAIn I WISH U a HApPy bIrtHday: DDDDD";
+    document.getElementById('Lang').innerHTML = "Language";
+    document.getElementById('tamil').innerHTML = "Tamil";
+    document.getElementById('french').innerHTML = "French";
+    document.getElementById('english').innerHTML = "English";
+}
+
+
